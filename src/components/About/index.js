f297a1a21ad959db024ef0794c97a577880a7cd1
Muhaimen Ahmed <muhaimen.ahmed@gmail.com>
2021-11-19 19:39:36 -0500

    created aboutme

---
+++ src/components/About/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function About() {
+
+    return(
+        <section className="container">
+            <h2 class="top-title">Muhaimen Ahmed</h2>
+            <hr></hr>
+            <div>
+                <img class="mb-5" id="avatar" src="https://avatars.githubusercontent.com/u/84346437?v=4" alt="Muhaimen Ahmed" />
+
+                <p>
+                Hi, my name is Muhaimen Ahmed, but most people call me Moo.  I am a budding full stack developer.  I will soon graduate from the Columbia Engineering Full Stack Bootcamp.
+                </p>
+                <p>
+                I have experience publishing apps that use the full MERN stack.  Have learned to work using the AGILE workflow and have picked up many other skills such as CSS, HTML, mySQL, Node, Mongodb, amongst many other skills applicable to the MERN stack.
+
+                Aside from coding I have a background in Political Science, Economics, and Computer Applications in Business. Some of my hobbies are weightlifting, photography, and content creation.
+                </p>
+                <p>
+                Whether its taking a nice photo, making a workout program to meet my body goals, or putting together a full stack application; one of my most important skills is my research skills.  I am always looking to improve myself and add to my skill set and am a valuable team member regardless of the context.
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default About;
